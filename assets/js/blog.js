972ee000110db1573a0b13f43b5a23b005dcbb23
unknown <cnm.coursework@gmail.com>
2024-03-24 23:04:03 -0600

    working localStorage setItem/getItem and setting to an array

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,7 +1,22 @@
-function renderBlogPosts() {
-    usernameInput.textContent = localStorage.getItem("username");
-    titleInput.textContent = localStorage.getItem("title");
-    contentInput.textContent = localStorage.getItem("content");
-}  
+// function renderBlogPosts() {
+//     // usernameInput.textContent = localStorage.getItem("username");
+//     // titleInput.textContent = localStorage.getItem("title");
+//     // contentInput.textContent = localStorage.getItem("content");
+//    blogEntries.textContent = blogJSON.parse(localStorage.getItem('blogPosts'))
+// }  
 
-renderBlogPosts();
+// renderBlogPosts();
+
+// loop through all blogs - for loop
+// create 3 elements - 1 for each blog post
+// add to the element - textContent
+// hardcode result to blog.html (html / css) - add for class elements then stlye
+// then come back here -- make sure it works -- take hard code (css / html) out 
+// then create that html info in java - the div - p - 
+// append the element
+// Access toggle switch HTML element
+
+// use document.onload 
+// get all local data from local data - line 13 on logic.js
+// create for loop to loop through the array of objects 
+// maniuplate the DOM to list all the blogs in blog.html
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,24 +4,31 @@ const contentInput = document.querySelector("#content");
 const submitInputButton = document.querySelector("#submit")
 
 
-submitInputButton.addEventListener("click", function(event) {
+submitInputButton.addEventListener("click", function (event) {
     event.preventDefault();
-
+    //sets users inputed values to variables
     const username = usernameInput.value;
     const title = titleInput.value;
     const content = contentInput.value;
 
-    
-    if (username === "" || title === "" || content === ""){
+    //prompt user to finish form before moving on   
+    if (username === "" || title === "" || content === "") {
         alert("Please complete form before submitting");
-    }else {
-        localStorage.setItem("username", JSON.stringify(username));
-        localStorage.setItem("title", JSON.stringify(title));
-        localStorage.setItem("content", JSON.stringify(content));
+    } else {
+
+        let blogPosts = {
+            username,
+            title,
+            content
+        }
+
+        let blogEntries = JSON.parse(localStorage.getItem("blogPosts")) || [];
+        blogEntries.push(blogPosts);
+
+        localStorage.setItem("blogPosts", JSON.stringify(blogEntries));
+        
         //directs me to where the posts are listed
         window.location.href = "blog.html";
     }
-
 });
-
 
